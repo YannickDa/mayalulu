4ad6cad48e3cf9c910d6b18b8f3f8d6f12b38120
Yannick <yannick@idvive.com>
2015-06-20 23:37:32 +0200

    Add jQuery reference

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 "use strict";
 
+var $ = require('jquery');
+
 module.exports = {
     app: require('./src/application'),
     model: require('./src/model'),
     component: require('./src/component'),
     view: require('./src/view'),
-    routes: require('./src/routes')
+    routes: require('./src/routes'),
+    $: $
 };
